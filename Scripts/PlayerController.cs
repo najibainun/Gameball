fdf5b704f4b906a34fd88923b2cc3d0e7dff379b
najibainun <najibainun283webmail@webmail.umm.ac.id>
2022-01-04 22:40:06 +0700

    Upload
    Upload file

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    private Rigidbody rb;
+    private int count;
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Ainun Najib 2019-335\nKelas : G\nCount : " + count.ToString();
+        if(count >=6)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider Other)
+    {
+        if(Other.gameObject.CompareTag("Pick Up"))
+        {
+             Other.gameObject.SetActive(false);
+             count = count + 1;
+
+             SetCountText();
+        }
+    }
+}
